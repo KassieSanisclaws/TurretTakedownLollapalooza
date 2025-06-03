a5e26fcbc93c13f2c70acfadf3deb380fd51266f
Kadeem Cherman <Kassie35dev@Gmail.com>
2025-06-03 18:05:56 -0400

    first commit of unreal engine first person shooter, project setings and structure.

---
+++ Source/TurretTakedownLollap/TurretTakedownLollap.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TurretTakedownLollap : ModuleRules
+{
+	public TurretTakedownLollap(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ Source/TurretTakedownLollap/TurretTakedownLollapGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TurretTakedownLollapGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATurretTakedownLollapGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATurretTakedownLollapGameMode();
+};
+
+
+
